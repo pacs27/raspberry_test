380bf10461d070ed796fab392d1db5cb01a26090
pacs27 <pacopuig1995@gmail.com>
2022-02-25 09:19:01 +0100

    updated app to http module

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
 
-app.use(express.static('public'));
+var dir = path.join(__dirname, 'public');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+var server = http.createServer(function (req, res) {
+    var reqpath = req.url.toString().split('?')[0];
+    if (req.method !== 'GET') {
+        res.statusCode = 501;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Method not implemented');
+    }
+    var file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
+    if (file.indexOf(dir + path.sep) !== 0) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.setHeader('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('error', function () {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Not found');
+    });
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+server.listen(8000, function () {
+    console.log('Listening on http://localhost:8000/');
+});
